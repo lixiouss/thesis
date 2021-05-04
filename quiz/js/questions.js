760dc3d2e1a06df43e44f840067fb368fccbec78
felix <eralixeralix@gmail.com>
2021-05-04 22:02:49 +0800

    first commit

---
+++ quiz/js/questions.js
@@ -0,0 +1,80 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "World time is standardized by how many clocks?",
+    answer: "80 atomic clocks",
+    options: [
+      "80 atomic clocks",
+      "70 atomic clocks",
+      "60 atomic clocks",
+      "90 atomic clocks"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In what year did the National Bereau of Standards announced the world's first atomic clock?",
+    answer: "1949",
+    options: [
+      "1959",
+      "1964",
+      "1994",
+      "1949"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How accurate is the NIST-F1?",
+    answer: "1 second every 20 million years",
+    options: [
+      "1 second every 10 million years",
+      "20 second every 1 million years",
+      "1 second every 20 million years",
+      "20 second every	2 million years"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The U.S. National Bureau of Standards is now called - ",
+    answer: "National Institute of Standards and Technology",
+    options: [
+      "National Institute of Science and Technology",
+      "National Institute of Standard and Time",
+      "National Institute of Science and Time",
+      "National Institute of Standards and Technology"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Select the one that doesn't belong to the group",
+    answer: "wrist watch",
+    options: [
+      "all satellite commmunications",
+      "powers the internet",
+      "wrist watch",
+      "GPS Devices"
+    ]
+  },
+    {
+    numb: 6,
+    question: "In how many countries are the atomic clocks scattered?",
+    answer: "24 countries",
+    options: [
+      "24 countries",
+      "34 countries",
+      "42 countries",
+      "43 countries"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Who maintains the Coordinated Universal Time?",
+    answer: "Bereau of Time",
+    options: [
+      "Bereau of Science",
+      "Bereau of Tecnology",
+      "Bereau of Standard",
+      "Bereau of Time"
+    ]
+  },
+];
---
+++ story/js/script.js
@@ -0,0 +1,24 @@
+//selecting all required elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+
+// if startQuiz button clicked
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); //show info box
+}
+
+// if exitQuiz button clicked
+exit_btn.onclick = ()=>{
+    window.location = "../home.html";
+}
+
+// if continueQuiz button clicked
+continue_btn.onclick = ()=>{
+    window.location = "../quiz/index.html";
+}
